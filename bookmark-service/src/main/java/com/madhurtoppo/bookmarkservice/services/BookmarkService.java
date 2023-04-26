224e463c23557be349a5c989e00e12a238743303
Madhur Toppo <madhur.toppo@gmail.com>
2023-04-27 02:05:57 +0530

    Add Pagination support for Bookmarks

--- bookmark-service/src/main/java/com/madhurtoppo/bookmarkservice/services/BookmarkService.java
+++ bookmark-service/src/main/java/com/madhurtoppo/bookmarkservice/services/BookmarkService.java
@@ -3,6 +3,9 @@ package com.madhurtoppo.bookmarkservice.services;
 import com.madhurtoppo.bookmarkservice.entities.Bookmark;
 import com.madhurtoppo.bookmarkservice.repositories.BookmarkRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +19,9 @@ public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
 
     @Transactional(readOnly = true)
-    public List<Bookmark> getBookmarks() {
-        return bookmarkRepository.findAll();
+    public List<Bookmark> getBookmarks(Integer page) {
+        int pageNo = page < 1 ? 0 : page - 1;
+        Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "creationTime");
+        return bookmarkRepository.findAll(pageable).getContent();
     }
 }
