18006c7e28358fe77cb95435172693302cf441ba
Madhur Toppo <madhur.toppo@gmail.com>
2023-04-27 13:33:47 +0530

    Add sql scripts to insert sample bookmarks data

---
+++ bookmark-service/src/main/resources/db/migration/postgresql/V2__insert_sample_data.sql
@@ -0,0 +1,18 @@
+INSERT INTO bookmarks (name, url, creation_time) VALUES
+    ('GithubMadhur', 'https://github.com/MadhurToppo', CURRENT_TIMESTAMP),
+    ('LinkedInMadhur', 'https://www.linkedin.com/in/madhurtoppo/', CURRENT_TIMESTAMP),
+    ('Spring', 'https://start.spring.io', CURRENT_TIMESTAMP),
+    ('Micronaut', 'https://micronaut.io', CURRENT_TIMESTAMP),
+    ('Quarkus', 'https://quarkus.io', CURRENT_TIMESTAMP),
+    ('Java', 'https://www.java.com', CURRENT_TIMESTAMP),
+    ('Microservices', 'https://microservices.io', CURRENT_TIMESTAMP),
+    ('Refactoring', 'https://www.refactoring.com', CURRENT_TIMESTAMP),
+    ('Apache Kafka', 'https://kafka.apache.org', CURRENT_TIMESTAMP),
+    ('Docker', 'https://www.docker.com', CURRENT_TIMESTAMP),
+    ('Kubernetes', 'https://kubernetes.io', CURRENT_TIMESTAMP),
+    ('Martin Fowler', 'https://martinfowler.com', CURRENT_TIMESTAMP),
+    ('Baeldung', 'https://www.baeldung.com', CURRENT_TIMESTAMP),
+    ('Javascript', 'https://www.javascript.com', CURRENT_TIMESTAMP),
+    ('React', 'https://react.dev', CURRENT_TIMESTAMP),
+    ('Next JS', 'https://nextjs.org', CURRENT_TIMESTAMP)
+;
