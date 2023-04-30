4d0bc1934c1050d687861743c43716cc57289790
Madhur Toppo <madhur.toppo@gmail.com>
2023-05-01 00:58:11 +0530

    Add NavBar component from Bootstrap template

--- bookmark-ui/pages/index.tsx
+++ bookmark-ui/pages/index.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import NavBar from "@/components/NavBar";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
+        <NavBar/>
       <h1>Welcome to Bookmark Application</h1>
       <button className={"btn btn-primary"}>Click</button>
     </div>
