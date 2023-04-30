7dbe355200f6bc81185a6f6990d564c31d946846
Madhur Toppo <madhur.toppo@gmail.com>
2023-05-01 02:11:39 +0530

    Finalize Navbar component, add styling

--- bookmark-ui/components/NavBar.tsx
+++ bookmark-ui/components/NavBar.tsx
@@ -1,42 +1,28 @@
 import React from 'react';
+import Link from "next/link";
 
 const NavBar = () => (
+  <div>
     <header>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse"
-                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div className="container-fluid">
+          <Link href="/" legacyBehavior><a className="navbar-brand">Bookmark</a></Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#navbarCollapse" aria-controls="navbarCollapse"
+                  aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <div className="dropdown-divider"></div>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" href="#">Disabled</a>
-                </li>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link href="/bookmarks/add" legacyBehavior><a className="nav-link">Add Bookmark</a></Link>
+              </li>
             </ul>
+          </div>
         </div>
-    </nav>
+      </nav>
     </header>
+  </div>
 );
 
 export default NavBar;
--- bookmark-ui/pages/_app.tsx
+++ bookmark-ui/pages/_app.tsx
@@ -1,7 +1,16 @@
 import '@/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from 'next/app'
+import NavBar from "@/components/NavBar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <NavBar/>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+
+  )
 }
