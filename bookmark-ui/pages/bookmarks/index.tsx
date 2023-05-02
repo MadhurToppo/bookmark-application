b9023504c4379c3ab350427bc97667600929f428
Madhur Toppo <madhur.toppo@gmail.com>
2023-05-03 03:09:25 +0530

    Refactor code to rename fields

--- bookmark-ui/pages/bookmarks/index.tsx
+++ bookmark-ui/pages/bookmarks/index.tsx
@@ -3,23 +3,23 @@ import {BookmarksResponse} from "@/services/models";
 import {fetchBookmarks} from "@/services/api";
 
 interface HomeProps {
-  bookmarks: BookmarksResponse
+  bookmarksResponse: BookmarksResponse
 }
 
 const Home: NextPage<HomeProps> = (props) => {
   return (
     <div>
       <h1>Welcome to Bookmark Application</h1>
-      {props.bookmarks.data.map(bookmark => <h2>{bookmark.name}</h2>)}
+      {props.bookmarksResponse.bookmarks.map(bookmark => <h2>{bookmark.name}</h2>)}
     </div>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const bookmarks = await fetchBookmarks()
+  const responseData = await fetchBookmarks()
   return {
     props: {
-      bookmarks
+      bookmarksResponse: responseData
     }
   }
 }
